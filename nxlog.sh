b35d9f7d8e28b19cc010cb2a6c932816308c32bd
Roman Titov <ffuugoo@users.noreply.github.com>
2022-07-23 00:06:18 +1000

    Add CMake support

--- nxlog.sh
+++ nxlog.sh
@@ -10,6 +10,8 @@ declare root=${repo:h}
 declare env=$root/.env
 
 declare nxlog=$root/nxlog
+declare build=$nxlog/build
+
 declare rust=$nxlog/rust
 
 declare addons=$root/nxlog-addons
@@ -34,13 +36,68 @@ function clone {
 }
 
 function configure {
-	if [[ -e $nxlog/Makefile ]]
+	if [[ -f $build/CMakeCache.txt || -f $nxlog/Makefile ]]
 	then
 		return 0
 	fi
 
 	clone
 
+	if [[ -f $nxlog/CMakeLists.txt && -z ${prepare:-} ]]
+	then
+		configure-cmake
+	else
+		configure-autotools
+	fi
+}
+
+function configure-cmake {
+	mkdir -p $build
+
+	cd $build
+
+	cmake .. \
+	\
+	-G ${${prepare:+'Unix Makefiles'}:-Ninja} \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_INSTALL_PREFIX=$env/nxlog \
+	\
+	${prepare:+-DCMAKE_EXPORT_COMPILE_COMMANDS=YES} \
+	\
+	-DNX_MAKE_DBI_MODULES=NO \
+	-DNX_MAKE_ODBC_MODULES=NO \
+	-DNX_MAKE_GO_MODULES=NO \
+	-DNX_MAKE_JAVA_MODULES=NO \
+	-DNX_MAKE_PERL_MODULES=NO \
+	-DNX_MAKE_PYTHON_MODULES=NO \
+	-DNX_MAKE_RUBY_MODULES=NO \
+	-DNX_MAKE_KAFKA_MODULES=NO \
+	-DNX_MAKE_ZMQ_MODULES=NO \
+	\
+	-DNX_MAKE_ACCT=NO \
+	-DNX_MAKE_BSM=NO \
+	-DNX_MAKE_CHECKPOINT_MODULE=NO \
+	-DNX_MAKE_KERNEL_MODULE=NO \
+	-DNX_MAKE_LINUXAUDIT=NO \
+	-DNX_MAKE_PCAP_MODULE=NO \
+	\
+	-DNX_MAKE_WSEVENTING_MODULE=NO \
+	\
+	-DNX_MAKE_MACES_MODULE=NO \
+	\
+	-DNX_MAKE_RUST=NO
+
+	compdb-list
+
+	cd $nxlog
+
+	if [[ -f $build/compile_commands.json ]]
+	then
+		ln -sf $build/compile_commands.json
+	fi
+}
+
+function configure-autotools {
 	cd $nxlog
 
 	if [[ ! -e configure ]]
@@ -69,38 +126,60 @@ function configure {
 		--disable-documentation \
 		--disable-hardening \
 		\
-		--disable-bsm \
-		--disable-checkpoint \
-		--disable-dbi \
-		--disable-im_maces \
-		--disable-im_pcap \
-		--disable-java \
-		--disable-kafka \
-		--disable-kerberos \
 		--disable-odbc \
+		--disable-dbi \
+		--disable-java \
 		--disable-xm_perl \
 		--disable-xm_python \
 		--disable-xm_ruby \
-		--disable-zmq
+		--disable-kafka \
+		--disable-zmq \
+		\
+		--disable-bsm \
+		--disable-checkpoint \
+		--disable-im_pcap \
+		\
+		--disable-kerberos \
+		\
+		--disable-im_maces
 }
 
 function prepare {
-	configure
+	prepare=1 configure
 
+	if [[ -f $build/CMakeCache.txt ]]
+	then
+		prepare-cmake
+	else
+		prepare-autotools
+	fi
+}
+
+function prepare-cmake {
+	cd $nxlog/build
+
+	ninja-make nx_shared
+}
+
+function prepare-autotools {
 	cd $nxlog
 
-	make -C src/common expr-tokens.c expr-grammar.c expr-grammar.h expr-core-funcproc.c expr-core-funcproc.h
+	make -C src/common \
+		libnx.la \
+		expr-tokens.c \
+		expr-grammar.c expr-grammar.h \
+		expr-core-funcproc.c expr-core-funcproc.h
 
 	if [[ ! -e compile_commands.json ]]
 	then
+		rm -f compile_commands.json
+
 		if whence -p compiledb &>/dev/null
 		then
 			compiledb --no-build -- make
 			compdb-list
 		fi
 	fi
-
-	make -C src/common libnx.la
 }
 
 function build {
@@ -108,6 +187,21 @@ function build {
 
 	configure
 
+	if [[ -f $build/CMakeCache.txt ]]
+	then
+		build-cmake
+	else
+		build-autotools
+	fi
+}
+
+function build-cmake {
+	cd $nxlog/build
+
+	ninja-make
+}
+
+function build-autotools {
 	cd $nxlog
 
 	if declare wrapper && wrapper="$(make-wrapper)" && [[ $wrapper != '' ]]
@@ -128,10 +222,26 @@ function install {
 
 	install=1 build
 
+	if [[ -f $build/CMakeCache.txt ]]
+	then
+		install-cmake
+	else
+		install-autotools
+	fi
+
+	mkdir -p $env/nxlog/var/{run,spool}/nxlog
+}
+
+function install-cmake {
+	cd $nxlog/build
+
+	ninja-make install
+}
+
+function install-autotools {
 	cd $nxlog
 
 	make install
-	mkdir -p $prefix/var/{run,spool}/nxlog
 }
 
 function distclean {
@@ -141,6 +251,11 @@ function distclean {
 	fi
 
 	cd $nxlog
+
+	if [[ -d build ]]
+	then
+		rm -r build
+	fi
 
 	if [[ -f Makefile ]]
 	then
@@ -153,11 +268,6 @@ function rust {
 	prepare
 
 	cd $rust
-
-	if [[ ! -e nxlog-module-sys/nxlog ]]
-	then
-		ln -sf $nxlog nxlog-module-sys/nxlog
-	fi
 
 	cargo build --all
 	cargo build -p nxlog-wrapper --bin nxlog-wrapper
@@ -286,6 +396,22 @@ function xcc-cleanup {
 	rm -rf $root/$xcc $root/$xcc.tar.gz
 }
 
+
+function ninja-make {
+	if [[ $* != install ]]
+	then
+		declare j1= jnproc=1
+	else
+		declare j1=1 jnproc=
+	fi
+
+	if [[ -e build.ninja ]]
+	then
+		ninja ${j1:+-j1} $@
+	else
+		make ${jnproc:+-j$(nproc)} $@
+	fi
+}
 
 function make-wrapper {
 	for wrapper in compiledb bear
